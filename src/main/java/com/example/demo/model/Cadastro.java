2d14bfa906fb7cdb70dcc93d3f8efc49f468e6be
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-17 09:18:46 -0300

    Implementação e utilização do Lombok na classe Cadastro

--- src/main/java/com/example/demo/model/Cadastro.java
+++ src/main/java/com/example/demo/model/Cadastro.java
@@ -2,11 +2,13 @@ package com.example.demo.model;
 
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.sql.Date;
 
 @Entity
 @Table(name = "CADASTRO")
+@Data
 public class Cadastro {
     @Id
     private long id;
@@ -26,91 +28,4 @@ public class Cadastro {
     private String tipoPlano;
     @Column(name = "cep")
     private String cep;
-
-    public Cadastro() {
-    }
-
-    public Cadastro(long id, String nome, String sobrenome, String email, String senha, Date dataNascimento, String sexo, String tipoPlano, String cep) {
-        this.id = id;
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-        this.email = email;
-        this.senha = senha;
-        this.dataNascimento = dataNascimento;
-        this.sexo = sexo;
-        this.tipoPlano = tipoPlano;
-        this.cep = cep;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public Date getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(Date dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
-
-    public String getTipoPlano() {
-        return tipoPlano;
-    }
-
-    public void setTipoPlano(String tipoPlano) {
-        this.tipoPlano = tipoPlano;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
 }
